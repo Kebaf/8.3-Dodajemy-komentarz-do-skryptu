b55d8e97ad6df6a3db9512733ec62f66ebf8ad59
ENTER <ENTER>
2017-03-29 20:46:51 +0200

    nowe repo z zadania 8.3

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var name = prompt('Wpisz swoje imię');
+// alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+/* jeden inny komentarz
+bardzo długi komentarz
+bardzo bardzo */
+//drugi inny komentarz
